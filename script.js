e4aa2da1dcadf23b9dc65ba6769d0bb514080690
Zak Monnet <zakmonnet@gmail.com>
2020-08-24 13:23:14 -0500

    added javascript and css styling to make start screen hidden and question screen visible when start button is clicked

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// Global variables
 let timerDisplay = document.querySelector(".timerValue");
 let startButton = document.querySelector(".start");
+let startScreen = document.querySelector("#startScreen");
+let questionScreen = document.querySelector("#questionScreen");
 
-
+// timer function counts down from totalSeconds, when it reaches zero an alert is displayed and timer function stops
 function timer() {
+    display();
     let totalSeconds = 100;
 
     let timer = setInterval(function() {
@@ -14,9 +18,15 @@ function timer() {
             clearInterval(timer);
             alert("Times up!")
         };
-    }, 1000);
-
-    
+    }, 1000);    
 };
 
+function display() {
+    startScreen.className = "hidden";
+    questionScreen.className = "visible";
+
+};
+
+// Start button will execute timer funciton when clicked
 startButton.addEventListener("click", timer);
+
