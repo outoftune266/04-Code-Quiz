c265370f491ae68c0f14687cdcc6606f30fb5ed6
Zak Monnet <zakmonnet@gmail.com>
2020-08-24 16:03:50 -0500

    moved question object to quesitons.js, added game over screen once last answer is clicked

--- script.js
+++ script.js
@@ -11,29 +11,7 @@ let answerThree = document.querySelector("#answerThree");
 let answerFour = document.querySelector("#answerFour");
 let answerFeedback = document.querySelector("#answerFeedback");
 let nextButton = document.querySelector("#nextButton");
-
 let number = 0;
-
-// Question Object
-let questions = [
-    {   questionNumber: "1",
-        questionContent: "Is this question one?",
-        answerOne: "No",
-        answerTwo: "Yes",
-        answerThree: "You've got to be kidding me",
-        answerFour: "I've had it up to here!",
-        correctAnswer: "answerTwo",
-    },
-
-    {   questionNumber: "2",
-        questionContent: "Is this question one?",
-        answerOne: "Yes",
-        answerTwo: "No",
-        answerThree: "You've got to be kidding me",
-        answerFour: "I've had it up to here!",
-        correctAnswer: "answerTwo",
-    }
-];
 
 // timer function counts down from totalSeconds, when it reaches zero an alert is displayed and timer function stops
 function timer() {
@@ -56,13 +34,13 @@ function timer() {
 
 //Pulls next question from questions object and displays it
 function advanceQuestion() {
+    nextButton.className = "hidden";
     questionNumber.innerHTML = questions[number].questionNumber;
     questionContent.innerHTML = questions[number].questionContent;
     answerOne.innerHTML = questions[number].answerOne;
     answerTwo.innerHTML = questions[number].answerTwo;
     answerThree.innerHTML = questions[number].answerThree;
     answerFour.innerHTML = questions[number].answerFour;
-    number++;
 };
 
 //Hides start screen and displays question screen
@@ -72,26 +50,38 @@ function display() {
 
 };
 
-/* needs question object built to complete logic
+//
 function answerSubmit() {
-    if () {
+    nextButton.className = "visible"
+    /*if ( .innerHTML === questions.correctAnswer) {
         answerFeedback.innerHTML = "Correct!";
     } else {
     answerFeedback.innerHTML = "Wrong!";
-    }
-    nextButton.className = "visible"
+    }*/
+    
+    if (number < 9){
+    number++;
+    }  else {
+        gameOver();
+    };
 };
-*/
+
+function gameOver() {
+    
+    alert("Thanx for playing!");
+};
+
 
 
 
 // Start button will execute timer funciton when clicked
 startButton.addEventListener("click", timer);
 
-//answerOne.addEventListener("click", answerSubmit);
-//answerTwo.addEventListener("click", answerSubmit);
-//answerThree.addEventListener("click", answerSubmit);
-//answerFour.addEventListener("click", answerSubmit);
+//Buttons will display next question button. Will also check if answer was correct and display "correct" or "wrong"
+answerOne.addEventListener("click", answerSubmit);
+answerTwo.addEventListener("click", answerSubmit);
+answerThree.addEventListener("click", answerSubmit);
+answerFour.addEventListener("click", answerSubmit);
 
 //This button will display the next question when clicked
 nextButton.addEventListener("click", advanceQuestion);
