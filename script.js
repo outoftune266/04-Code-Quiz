eded50b885576892cdc4687d050b295d28bef27a
Zak Monnet <zakmonnet@gmail.com>
2020-08-24 11:12:56 -0500

    added javascript for timer

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let questions = [
-    q: 
-    a1
-    a2
-    a3
-    a4
+let timerDisplay = document.querySelector(".timerValue");
+let startButton = document.querySelector(".start");
 
-]
+
+function timer() {
+    let totalSeconds = 100;
+
+    let timer = setInterval(function() {
+        totalSeconds--;
+
+        timerDisplay.textContent = totalSeconds;
+
+        if (totalSeconds === 0) {
+            clearInterval(timer);
+            alert("Times up!")
+        };
+    }, 1000);
+
+    
+};
+
+startButton.addEventListener("click", timer);
